653bcc092cc6143e907ab23ded4d1a1dce60ae67
Damien Pobel <dp@ez.no>
2016-02-26 13:20:11 +0100

    Completed the unit tests

--- test/github-user-events-tests.js
+++ test/github-user-events-tests.js
@@ -1,9 +1,9 @@
-/* global describe, before, beforeEach, afterEach, flush, it, fixture, assert, sinon */
+/* global describe, beforeEach, afterEach, flush, it, fixture, assert, sinon, Polymer */
 describe('github-user-events', function () {
     var element, server,
         username = 'dpobel';
 
-    function configureServerResponse(server, code, response) {
+    function configureServerResponse(server, code, response, username) {
         server.respondWith(
             'GET',
             `https://api.github.com/users/${username}/events`,
@@ -26,22 +26,25 @@ describe('github-user-events', function () {
         describe('loading', function () {
             it('should be true while loading', function () {
                 assert.isTrue(element.loading);
+                assert.strictEqual(element.getAttribute('loading'), "");
             });
 
             it('should be false when getting a successful response', function (done) {
-                configureServerResponse(server, 200, []);
+                configureServerResponse(server, 200, [], username);
                 server.respond();
                 flush(function () {
                     assert.isFalse(element.loading);
+                    assert.isNull(element.getAttribute('loading'));
                     done();
                 });
             });
 
             it('should be false when getting an error response', function (done) {
-                configureServerResponse(server, 404, []);
+                configureServerResponse(server, 404, [], username);
                 server.respond();
                 flush(function () {
                     assert.isFalse(element.loading);
+                    assert.isNull(element.getAttribute('loading'));
                     done();
                 });
             });
@@ -50,24 +53,27 @@ describe('github-user-events', function () {
         describe('error', function () {
             it('should be false by default', function () {
                 assert.isFalse(element.error);
+                assert.isNull(element.getAttribute('error'));
             });
 
             it('should be falsy with a successful response', function (done) {
-                configureServerResponse(server, 200, []);
+                configureServerResponse(server, 200, [], username);
                 server.respond();
 
                 flush(function () {
                     assert.notOk(element.error);
+                    assert.isNull(element.getAttribute('error'));
                     done();
                 });
             });
 
             it('should be truthy with an error response', function (done) {
-                configureServerResponse(server, 404, []);
+                configureServerResponse(server, 404, [], username);
                 server.respond();
 
                 flush(function () {
                     assert.ok(element.error);
+                    assert.strictEqual(element.getAttribute('error'), "");
                     done();
                 });
             });
@@ -89,7 +95,7 @@ describe('github-user-events', function () {
                 it('should unify events', function (done) {
                     var event;
 
-                    configureServerResponse(server, 200, [rawEvent]);
+                    configureServerResponse(server, 200, [rawEvent], username);
                     server.respond();
 
                     flush(function () {
@@ -103,6 +109,46 @@ describe('github-user-events', function () {
                         done();
                     });
                 });
+            });
+        });
+
+        describe('username', function () {
+            it('should refetch the events based on the username', function (done) {
+                var newUsername = 'user',
+                    rawEvent = {
+                        type: 'UpdateUserEvent',
+                    };
+
+                configureServerResponse(server, 200, [rawEvent], newUsername);
+                element.username = newUsername;
+                server.respondImmediately = true;
+                server.respond();
+
+                flush(function () {
+                    assert.equal(element.getAttribute('username'), newUsername);
+                    assert.isFalse(element.loading);
+                    assert.equal(element.events[0].type, 'Update User');
+                    done();
+                });
+            });
+
+        });
+    });
+
+    describe('dom', function () {
+        it('should generate an item per returned event', function (done) {
+            var rawEvent = {
+                    type: 'RunningUnitTestEvent',
+                },
+                response = [rawEvent, rawEvent, rawEvent];
+
+            configureServerResponse(server, 200, response, username);
+            server.respond();
+            flush(function () {
+                var items = Polymer.dom(element.root).querySelectorAll('paper-item');
+
+                assert.equal(items.length, response.length + 2);
+                done();
             });
         });
     });
